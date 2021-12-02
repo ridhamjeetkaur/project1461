c186f433692f71128b18a3e9ed38a58464f0893c
Ridhamjeet Kaur <90550256+ridhamjeetkaur@users.noreply.github.com>
2021-12-02 18:31:56 +0530

    Update career_guide_system_in_python.py

--- career_guide_system_in_python.py
+++ career_guide_system_in_python.py
@@ -8,16 +8,6 @@ from sklearn.linear_model import LogisticRegression
 import time
 from   sklearn.model_selection import train_test_split
 
-st.session_state['answer'] = ''
-
-st.write(st.session_state)
-
-realans = ['', 'abc', 'edf']
-
-if  st.session_state['answer'] in realans:
-    answerStat = "correct"
-elif st.session_state['answer'] not in realans:
-    answerStat = "incorrect"
 
 st.write(st.session_state)
 st.write(answerStat)
